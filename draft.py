70135db040003e66ec32715d20e3bf066dc5ebd2
Marat Bekmyrza <marat.bekmyrza@nu.edu.kz>
2020-11-06 01:39:04 +0600

    added bptt image

--- draft.py
+++ draft.py
@@ -1,28 +1,49 @@
+# here I've tried to implement the BPTT from Pascanu, 2014
+
 import torch
 
 
-
 class RNN:
-    def __init__(self, dim=1):
-        self.W_hx = torch.zeros(dim)
-        self.W_hh = torch.zeros(dim)
-        self.W_hy = torch.zeros(dim)
-        self.b_h = torch.zeros(dim)
-        self.b_y = torch.zeros(dim)
+    def __init__(self, input_size, dim):
+        self.input_size = input_size
+        self.dim = dim
 
-        self.h = torch.zeros(dim)
-        self.y = torch.zeros(dim)
+        self.W_hx = torch.ones(self.dim)  # dummy initialization
+        self.W_hh = torch.ones(self.dim)
+        self.W_hy = torch.ones(self.dim)
+        self.b_h = torch.ones(self.dim)
+        self.b_y = torch.ones(self.dim)
 
-    def forward(self, x, h):
-        self.h = torch.sigmoid(self.W_hx * x + self.W_hh * h + self.b_h)
-        self.y = torch.sigmoid(self.W_hy * self.h + self.b_y)
+        self.h = torch.zeros(self.input_size)  # same length as the input
+        self.y = torch.zeros(self.input_size)
+
+    def forward(self, x):
+        T = self.input_size  # T = len(x)
+        h_prev = torch.zeros()
+        for t in range(T):
+            self.h[t] = torch.sigmoid(self.W_hx * x[t] + self.W_hh * h_prev + self.b_h)
+            self.y[t] = torch.sigmoid(self.W_hy * self.h + self.b_y)
+            h_prev = self.h
+        return self.y, self.h
 
     def backward(self):
-        
+        T = self.input_size
+        gW_hy = grad(loss[T], W_hy)
+        gh = grad(loss[T], self.h[T])
+        gW_hx = gh * imgrad(self.h[T], W_hx)
+        gW_hh = gh * imgrad(self.h[T], W_hh)
+
+        for t in range(T-1, 0, -1):
+            gh = gh * grad(self.h[t+1], self.h[t]) + grad(loss[t], self.h[t])
+            gW_hy = gW_hy + grad(loss[t], W_hy)
+            gW_hx = gW_hx + gh * imgrad(self.h[t], W_hx)
+            gW_hh = gW_hh + gh * imgrad(self.h[t], W_hx)
 
 
 
-print(torch.zeros(1))
+
+
+print(torch.matmul(torch.tensor([2.0, 2.0]), torch.tensor([[1.0, 4.0], [3.0, 3.0]])))
 
 
 """
